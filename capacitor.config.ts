c23b346754215b066965c18f37ab10f8a90ae591
Victor Celis <victor.celis.c88@gmail.com>
2024-10-07 13:14:10 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Match_Inmobiliario',
+  webDir: 'www'
+};
+
+export default config;
