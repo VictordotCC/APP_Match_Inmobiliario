e278456282339ff8be697ac5eee32818ca91b695
Victor Celis <victor.celis.c88@gmail.com>
2024-10-23 13:17:52 -0300

    funcionalidad cards

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import * as Leaflet from 'leaflet';
 import { GlobalDataService } from '../servicios/global-data.service';
 import { Animation, GestureController, Gesture, AnimationController, GestureDetail, IonCard } from '@ionic/angular';
@@ -8,17 +8,17 @@ import { Animation, GestureController, Gesture, AnimationController, GestureDeta
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
-  @ViewChild('card', {static: false}) card: ElementRef | undefined;
+export class Tab3Page implements AfterViewInit{
+  @ViewChildren('card', {read: ElementRef}) cards: QueryList<ElementRef> | undefined;
 
-  animation: Animation | undefined;
+  animations: Animation[] = [];
   gesture : Gesture | undefined;
   animStarted = false;
   initialStep = 0;
   readonly MAX_TRANSLATE = 344 - 100 - 32;
 
   showDetails = false;
-  map: Leaflet.Map | undefined;
+  maps: Leaflet.Map[] = [];
   markers: Leaflet.Marker[] = [];
 
   paginationConfig = {
@@ -35,54 +35,53 @@ export class Tab3Page {
   //GESTOS
 
   setupSwipeGesture(){
-    if (this.card){
-      this.animation = this.animationCtrl.create('swipe-animation')
-      .addElement(this.card.nativeElement)
+    this.cards!.forEach((card, index) => {
+      const animation = this.animationCtrl.create('swipe-animation')
+      .addElement(card.nativeElement)
       .duration(100)
-      
-      const gesture: Gesture = this.gestureCtrl.create({
-        el: this.card.nativeElement,
+
+      this.animations.push(animation);
+
+      const gesture = this.gestureCtrl.create({
+        el: card.nativeElement,
         gestureName: 'swipe-like-dislike',
         threshold: 0,
-        onMove: (ev) => this.onMove(ev),
-        onEnd: (ev) => this.onEnd(ev)
+        onMove: (ev) => this.onMove(ev, index),
+        onEnd: (ev) => this.onEnd(ev, index)
       });
-      gesture.enable();
-    }
+    gesture.enable();
+    });
   }
 
-  onMove(ev: GestureDetail){
+  onMove(ev: GestureDetail, index: number){
     const container = ev.event.target as HTMLElement;
-    if (container.tagName === 'ION-IMG' || container.tagName === 'SWIPER-CONTAINER' || container.tagName === 'DIV'
+    if (!this.animations[index] || container.tagName === 'ION-IMG' || container.tagName === 'SWIPER-CONTAINER' || container.tagName === 'DIV'
       || this.showDetails){
       return;
     }
-    if (!this.animation){
-      return;
-    }
     if (!this.animStarted){
-      this.animation.progressStart();
+      this.animations[index].progressStart();
       this.animStarted = true;
     }
     const step = this.getStep(ev);
     const translateXvalue = ev.deltaX
-    this.animation.progressStep(step);
-    this.card!.nativeElement.style.transform = `translateX(${translateXvalue}px)`;
-    this.card!.nativeElement.style.filter = `brightness(${1 - Math.abs(step)/2})`;
+    this.animations[index].progressStep(step);
+    this.cards!.get(index)!.nativeElement.style.transform = `translateX(${translateXvalue}px)`;
+    this.cards!.get(index)!.nativeElement.style.rotate = `${translateXvalue/35}deg`;
+    this.cards!.get(index)!.nativeElement.style.filter = `brightness(${Math.max(1 - Math.abs(step)/2, 0.5)})`;
   }
 
-  onEnd(ev: GestureDetail){
-    if (!this.animation){
-      return;
-    }
-    if (!this.animStarted){
+  onEnd(ev: GestureDetail, index: number){
+    if (!this.animations[index] || !this.animStarted){
       return;
     }
     this.gesture?.enable(false)
     const step = this.getStep(ev);
 
-    this.animation.progressEnd(0, step).onFinish(() => {
-      this.card!.nativeElement.style.transform = 'translateX(0)';
+    this.animations[index].progressEnd(0, step).onFinish(() => {
+      this.cards!.get(index)!.nativeElement.style.transform = 'translateX(0)';
+      this.cards!.get(index)!.nativeElement.style.rotate = '0deg';
+      this.cards!.get(index)!.nativeElement.style.filter = '';
       //this.gesture?.enable(true);
     });
     console.log(ev.deltaX)
@@ -92,7 +91,6 @@ export class Tab3Page {
       this.rechazarPref('dislike');
     }
     this.animStarted = false;
-    this.card!.nativeElement.style.filter = '';
   }
 
   clamp(min: number, n: number, max: number){
@@ -106,34 +104,34 @@ export class Tab3Page {
 
   //MAPA
 
-  initializeMap() {
+  initializeMap(mapTag : HTMLElement) {
     const defaultIcon = Leaflet.icon({
       iconUrl: '../../assets/markers/marker-icon.png',
       shadowUrl: '../../assets/markers/marker-shadow.png'})
-    this.map = Leaflet.map('mapMatch').setView([this.datosGlobales.lat, this.datosGlobales.lon], this.datosGlobales.mapZoom);
+    const map = Leaflet.map(mapTag).setView([this.datosGlobales.lat, this.datosGlobales.lon], this.datosGlobales.mapZoom);
     Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors'
-    }).addTo(this.map);
-    this.adduserMarker(this.datosGlobales.lat, this.datosGlobales.lon, defaultIcon);
+    }).addTo(map);
+    this.adduserMarker(map, this.datosGlobales.lat, this.datosGlobales.lon, defaultIcon);
     const marker = Leaflet.marker([this.datosGlobales.lat -0.001, this.datosGlobales.lon -0.001], {icon: Leaflet.icon({ //TODO: obtener lat y lon del domicilio seleccionado
       iconUrl: '../../assets/markers/casa-with-shadow.png',
       iconSize: [36, 36],
       iconAnchor: [12, 41],
       popupAnchor: [1, -34],
-      })}).addTo(this.map);
+      })}).addTo(map);
     this.markers.push(marker);
   }
 
-  adduserMarker(lat: number, lon: number, icon: Leaflet.Icon) {
-    if (this.map) {
-      const marker = Leaflet.marker([lat, lon], {icon: icon}).addTo(this.map);
+  adduserMarker(map: Leaflet.Map, lat: number, lon: number, icon: Leaflet.Icon) {
+    if (map) {
+      const marker = Leaflet.marker([lat, lon], {icon: icon}).addTo(map);
       this.markers.push(marker);
     }
   }
 
-  destroyMap() {
-    if (this.map) {
-      this.map.remove();
+  destroyMap(map: Leaflet.Map) {
+    if (map) {
+      map.remove();
     }
   }
 
@@ -149,15 +147,26 @@ export class Tab3Page {
 
   //MOSTRAR DETALLES
 
-  toggleDetails(){
+  toggleDetails(ev: any){
+    const match_card = ev.closest('.match-card')
+    const hidden_info = match_card.querySelector('.hidden-info')
+    const mapMatch = match_card.querySelector('#mapMatch') as HTMLElement;
+    const info = match_card.querySelector('.info')
     this.showDetails = !this.showDetails;
 
     if (this.showDetails){
+      hidden_info.style.display = 'block';
+      info.style.display = 'none';
       setTimeout(() => {
-        this.initializeMap();
+        this.initializeMap(mapMatch);
       }, 0);
     } else {
-      this.destroyMap();
+      hidden_info.style.display = 'none';
+      info.style.display = 'block';
+      const map = this.maps.pop();
+      if (map){
+        this.destroyMap(map);
+      }
     }
   }
 }
