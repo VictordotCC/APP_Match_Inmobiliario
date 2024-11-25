89647899e67f08fe1f5f1af633d0a872fdaa8e13
Victor Celis <victor.celis.c88@gmail.com>
2024-11-25 16:50:58 -0300

    confirmacion correo

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -215,33 +215,19 @@ export class LoginPage  implements OnInit{
           this.preferencias.notificaciones = data.preferencias.notificaciones;
           await this.storage.set('preferencias', this.preferencias);
           console.log('token despues', await this.storage.get('access_token'));
-          //TODO: Eliminar lo que sigue al implementar lectura desde Storage
-          /*
-          this.datosGlobales.preferencias.usuario = this.datosGlobales.userGlobal;
-          this.datosGlobales.preferencias.busqueda_automatica = data.preferencias.busqueda_automatica;
-          this.datosGlobales.preferencias.distancia = data.preferencias.distancia;
-          this.datosGlobales.preferencias.tipo_operacion = data.preferencias.tipo_operacion;
-          this.datosGlobales.preferencias.tipo_vivienda = data.preferencias.tipo_vivienda;
-          this.datosGlobales.preferencias.condicion = data.preferencias.condicion;
-          this.datosGlobales.preferencias.area_total = data.preferencias.area_total;
-          this.datosGlobales.preferencias.pisos = data.preferencias.pisos;
-          this.datosGlobales.preferencias.area_construida = data.preferencias.area_construida
-          this.datosGlobales.preferencias.antiguedad = data.preferencias.antiguedad;
-          this.datosGlobales.preferencias.tipo_valor = data.preferencias.tipo_valor;
-          this.datosGlobales.preferencias.precio_minimo = data.preferencias.precio_minimo;
-          this.datosGlobales.preferencias.precio_maximo = data.preferencias.precio_maximo;
-          this.datosGlobales.preferencias.tipo_subsidio = data.preferencias.tipo_subsidio;
-          this.datosGlobales.preferencias.habitaciones = data.preferencias.habitaciones;
-          this.datosGlobales.preferencias.banos = data.preferencias.banos;
-          this.datosGlobales.preferencias.estaciona = data.preferencias.estaciona;
-          this.datosGlobales.preferencias.bodega = data.preferencias.bodega;
-          this.datosGlobales.preferencias.contactado = data.preferencias.contactado;
-          this.datosGlobales.preferencias.notificaciones = data.preferencias.notificaciones;
-          */
           this.router.navigate(['/tabs']);
         });
         //this.navCtrl.navigateForward(['/tabs']);
       } else if (data.status == 401){
+        console.log('usuario inactivo', data);
+        if (data.activo == false){
+          const alert = await this.alertController.create({
+            header: 'Error de logueo',
+            message: 'Usuario inactivo, Debe confirmar su correo',
+            buttons: ['Aceptar']
+          });
+          await alert.present();
+        } else {
         const alert = await this.alertController.create({
           header: 'Error de logueo',
           message: 'El usuario no existe o la contraseña es incorrecta',
@@ -249,6 +235,7 @@ export class LoginPage  implements OnInit{
         });
         await alert.present();
         //this.limpiarFormulario();
+      }
       }
       //this.limpiarFormulario();
     },
